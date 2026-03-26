64a2aacaf74dbb60b6b23080ed44c49346105df4
darzrm <darzrm@gmail.com>
2026-03-26 08:31:58 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// toggle function
+const elementToggleFunc = function (elem) { 
+  elem.classList.toggle("active"); 
+}
+
+// sidebar
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+if (sidebarBtn) {
+  sidebarBtn.addEventListener("click", function () { 
+    elementToggleFunc(sidebar); 
+  });
+}
+
+// testimonials modal (biarin, ga ganggu gallery)
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+testimonialsItem.forEach(item => {
+  item.addEventListener("click", function () {
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
+    modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
+    testimonialsModalFunc();
+  });
+});
+
+if (modalCloseBtn) modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+if (overlay) overlay.addEventListener("click", testimonialsModalFunc);
+
+// navigation
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+navigationLinks.forEach((link, index) => {
+  link.addEventListener("click", function () {
+    pages.forEach((page, i) => {
+      if (this.innerHTML.toLowerCase() === page.dataset.page) {
+        page.classList.add("active");
+        navigationLinks[i].classList.add("active");
+      } else {
+        page.classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    });
+    window.scrollTo(0, 0);
+  });
+});
